5280c19ac4447aaf7813ee1c488345ca0fcef058
aollivierre <abdullahollivierre@gmail.com>
2024-10-01 11:54:58 -0700

    moving files out

--- DeviceMigration/Secretsv2-with-PAT-and-Cert-RSA-and-AES-Hybrid/verify-Cert-Private-Key.ps1
+++
@@ -1,8 +0,0 @@
-$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-$cert.Import("C:\temp2\certs\cert.pfx", "Absurd0-Acting-Fetal-Relapsing-Sanctuary-Bulb-Startup-Cottage-Helper-Detached-Tumble-Exponent-Earmuff-Resume-Purging-Scoured-Wharf-Nicotine-Smudgy-Storm", [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::PersistKeySet)
-
-if ($cert.HasPrivateKey) {
-    Write-Host "The certificate contains a private key."
-} else {
-    Write-Host "The certificate does not contain a private key."
-}
