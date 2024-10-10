fb0ca156efde4ab6236da661dcfcc56f8494992c
aollivierre <abdullahollivierre@gmail.com>
2024-10-10 07:19:55 -0700

    reset module path

--- DeviceMigration/DeviceMigration.ps1
+++ DeviceMigration/DeviceMigration.ps1
@@ -106,6 +106,53 @@ function Write-AADMigrationLog {
     }
 }
 
+function Reset-ModulePaths {
+    [CmdletBinding()]
+    param ()
+
+    begin {
+        # Initialization block, typically used for setup tasks
+        Write-AADMigrationLog -Message "Initializing Reset-ModulePaths function..." -Level "DEBUG"
+    }
+
+    process {
+        try {
+            # Log the start of the process
+            Write-AADMigrationLog -Message "Resetting module paths to default values..." -Level "INFO"
+
+            # Get the current user's Documents path
+            $userModulesPath = [System.IO.Path]::Combine($env:USERPROFILE, 'Documents\WindowsPowerShell\Modules')
+
+            # Define the default module paths
+            $defaultModulePaths = @(
+                "C:\Program Files\WindowsPowerShell\Modules",
+                $userModulesPath,
+                "C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
+            )
+
+            # Attempt to reset the PSModulePath environment variable
+            $env:PSModulePath = [string]::Join(';', $defaultModulePaths)
+            Write-AADMigrationLog -Message "PSModulePath successfully set to: $($env:PSModulePath -split ';' | Out-String)" -Level "INFO"
+
+            # Optionally persist the change for the current user
+            [Environment]::SetEnvironmentVariable("PSModulePath", $env:PSModulePath, [EnvironmentVariableTarget]::User)
+            Write-AADMigrationLog -Message "PSModulePath environment variable set for the current user." -Level "INFO"
+        }
+        catch {
+            # Capture and log any errors that occur during the process
+            $errorMessage = $_.Exception.Message
+            Write-AADMigrationLog -Message "Error resetting module paths: $errorMessage" -Level "ERROR"
+
+            # Optionally, you could throw the error to halt the script
+            throw $_
+        }
+    }
+
+    end {
+        # Finalization block, typically used for cleanup tasks
+        Write-AADMigrationLog -Message "Reset-ModulePaths function completed." -Level "DEBUG"
+    }
+}
 
 
 
@@ -218,6 +265,9 @@ try {
             # Check if running in PowerShell 5
             if ($PSVersionTable.PSVersion.Major -ne 5) {
                 Write-AADMigrationLog -Message "Not running in PowerShell 5. Relaunching the command with PowerShell 5."
+
+                # Reset Module Paths when switching from PS7 to PS5 process
+                Reset-ModulePaths
 
                 # Get the path to PowerShell 5 executable
                 $ps5Path = "$Env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
